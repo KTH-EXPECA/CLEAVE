e86112a481b3df527e12313d9ff3c28ff90521b9
Manuel Olguín Muñoz <manuel@olguin.se>
2020-06-22 18:43:08 -0400

    documenting

--- cleave/client/plant.py
+++ cleave/client/plant.py
@@ -16,7 +16,7 @@ from __future__ import annotations
 import time
 from abc import ABC, abstractmethod
 from multiprocessing import Event, RLock
-from typing import Callable, Dict, Optional
+from typing import Any, Callable, Dict, Optional
 
 from loguru import logger
 
@@ -62,6 +62,18 @@ class Plant:
                  init_state: BaseState,
                  sensor: BaseSensor,
                  actuator: BaseActuator):
+        """
+        Parameters
+        ----------
+        dt
+            Time interval in seconds between successive simulation steps.
+        init_state
+            Initial plant state.
+        sensor
+            BaseSensor instance associated with the plant.
+        actuator
+            BaseActuator instance associated with the plant.
+        """
         super(Plant, self).__init__()
         logger.debug('Initializing plant.', enqueue=True)
 
@@ -82,25 +94,79 @@ class Plant:
         self._end_of_step_hooks = HookCollection()
         self._pre_sim_hooks = HookCollection()
 
-    def hook_start_of_step(self, fn: Callable[[...], ...]):
+    def hook_start_of_step(self, fn: Callable[[], ...]):
+        """
+        Register a callable to be called at the beginning of each simulation
+        step. This callable should take no arguments.
+
+        The intended use pattern for this method is as a decorator.
+
+        Parameters
+        ----------
+        fn
+            Callable to be invoked at the start of each simulation step.
+        """
         self._start_of_step_hooks.add(fn)
 
-    def hook_end_of_step(self, fn: Callable[[...], ...]):
+    def hook_end_of_step(self, fn: Callable[[], ...]):
+        """
+        Register a callable to be called at the end of each simulation
+        step. This callable should take no arguments.
+
+        The intended use pattern for this method is as a decorator.
+
+        Parameters
+        ----------
+        fn
+            Callable to be invoked at the end of each simulation step.
+        """
         self._end_of_step_hooks.add(fn)
 
-    def hook_pre_sim(self, fn: Callable[[...], ...]):
+    def hook_pre_sim(self, fn: Callable[[Any], ...]):
+        """
+        Register a callable to be called immediately before advancing the
+        state of the simulation, but after the initial procedures of each
+        simulation step. This callable should take an optional `actuation`
+        keyword argument through which it will receive the actuation command
+        about to be applied to the state.
+
+        The intended use pattern for this method is as a decorator.
+
+        Parameters
+        ----------
+        fn
+            Callable to be invoked right before advancing the simulation state.
+        """
+
         self._pre_sim_hooks.add(fn)
 
     def shutdown(self):
+        """
+        Shuts down this plant.
+        """
+
         # TODO: might need to do more stuff here at some point
         logger.warning('Shutting down plant.', enqueue=True)
         self._shutdown_event.set()
 
     def sample_state(self) -> BaseState:
+        """
+        Returns the current state of the plant. Thread- and process-safe.
+
+        Returns
+        -------
+        BaseState
+            Current state of the plant.
+        """
         with self._state_lck:
             return self._state
 
     def _step(self):
+        """
+        Executes all the necessary procedures to advance the simulation a
+        single discrete time step. This method calls the respective hooks,
+        polls the actuator, advances the state and updates the actuator.
+        """
         self._start_of_step_hooks.call()
 
         # pull next actuation command from actuator
