b361c5611630084a78d020a150cb1cffecb01775
Manuel Olguín Muñoz <manuel@olguin.se>
2020-07-05 13:19:26 -0400

    sensor loop

--- cleave/client/sensor.py
+++ cleave/client/sensor.py
@@ -14,9 +14,14 @@
 
 from __future__ import annotations
 
-from typing import Any, Callable, Type, Union
+from multiprocessing.synchronize import SemLock
+from typing import Any, Callable, List, Type, Union
 
+from multiprocess.context import _default_context
 from multiprocess.sharedctypes import RawValue
+from multiprocess.synchronize import Semaphore
+
+from cleave.client.mproc import RunnableLoop
 
 _ctype_mappings = {
     int  : 'q',  # signed long long
@@ -39,7 +44,7 @@ class Sensor:
     def __init__(self,
                  prop_name: str,
                  prop_type: SENSOR_TYPES,
-                 sampling_freq_hz: float,
+                 sampling_freq_hz: int,
                  noise_fn: Callable[[SENSOR_TYPES],
                                     SENSOR_TYPES] = lambda x: x):
         if prop_type not in _ctype_mappings:
@@ -56,7 +61,7 @@ class Sensor:
         return self._prop_name
 
     @property
-    def sampling_frequency(self) -> float:
+    def sampling_frequency(self) -> int:
         return self._sampl_freq
 
     def write_value(self, value: Any):
@@ -64,3 +69,36 @@ class Sensor:
 
     def read_value(self) -> Any:
         return self._noise(self._value.value)
+
+
+class SensorLoop(RunnableLoop):
+    def __init__(self,
+                 plant_upd_freq_hz: int,
+                 sensors: List[Sensor],
+                 shared_lock: SemLock):
+        super(SensorLoop, self).__init__()
+        self._cycle_triggers = dict()
+        for sensor in sensors:
+            cycles_per_upd = plant_upd_freq_hz // sensor.sampling_frequency
+            for c in range(0, plant_upd_freq_hz, cycles_per_upd):
+                if c not in self._cycle_triggers:
+                    self._cycle_triggers[c] = []
+                self._cycle_triggers[c].append(sensor)
+
+        self._plant_freq = plant_upd_freq_hz
+        self._cycles = -1
+        self._cycle_sem = Semaphore(value=0, ctx=_default_context)
+        self._lock = shared_lock
+
+    def tick(self):
+        self._cycle_sem.release()
+
+    def _loop(self) -> None:
+        self._cycle_sem.acquire()
+        with self._lock:
+            self._cycles += 1
+            # get values from sensors triggered in this cycle
+            for sensor in self._cycle_triggers.get(self._cycles, []):
+                value = sensor.read_value()
+
+                # TODO do something with value
