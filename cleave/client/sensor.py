46c84a3e8a7646688001067561a06294a58ad257
Manuel Olguín Muñoz <manuel@olguin.se>
2020-07-14 15:17:49 -0400

    simplify SensorArray to simply process sensor data, not send it

--- cleave/client/sensor.py
+++ cleave/client/sensor.py
@@ -2,12 +2,10 @@ from __future__ import annotations
 
 import warnings
 from abc import ABC, abstractmethod
-from threading import RLock
-from typing import Dict
+from typing import Collection, Dict
 
-from ..network import ClientCommHandler
 from ..util import IncompatibleFrequenciesError, MissingPropertyError, \
-    RegisteredSensorWarning, PhyPropType
+    PhyPropType, RegisteredSensorWarning
 
 
 class Sensor(ABC):
@@ -35,39 +33,16 @@ class SimpleSensor(Sensor):
 
 
 class SensorArray:
-    def __init__(self, plant_freq: int, comm: ClientCommHandler):
+    def __init__(self, plant_freq: int,
+                 sensors: Collection[Sensor]):
         super(SensorArray, self).__init__()
         self._plant_freq = plant_freq
         self._prop_sensors = dict()
         self._cycle_triggers = dict()
         self._cycle_count = 0
-        self._lock = RLock()
-        self._comm = comm
 
-    def _recalculate_cycle_triggers(self):
-        """
-        Internal helper method to recalculate cycle triggers
-
-        Example: if a 200 Hz sensor is attached to a plant which updates at
-        600 Hz, that means that for each sensor cycle, 3 plant cycles need to
-        have passed. So, using the plant cycle count as reference, the sensor
-        needs to sample at cycles:
-        [0, 3, 6, 8, 12, ..., 600]
-        """
-
-        with self._lock:
-            self._cycle_triggers = dict()
-            for _, sensor in self._prop_sensors.items():
-                pcycles_per_scycle = \
-                    self._plant_freq // sensor.sampling_frequency
-                for trigger in range(0, self._plant_freq, pcycles_per_scycle):
-                    if trigger not in self._cycle_triggers:
-                        self._cycle_triggers[trigger] = []
-
-                    self._cycle_triggers[trigger].append(sensor)
-
-    def add_sensor(self, sensor: Sensor):
-        with self._lock:
+        # assign sensors to properties
+        for sensor in sensors:
             if sensor.measured_property_name in self._prop_sensors:
                 warnings.warn(f'Replacing already registered sensor for '
                               f'property {sensor.measured_property_name}',
@@ -84,29 +59,41 @@ class SensorArray:
                 )
 
             self._prop_sensors[sensor.measured_property_name] = sensor
-            self._recalculate_cycle_triggers()
 
-    def update_property_values(self, prop_values: Dict[str, PhyPropType]):
-        with self._lock:
-            try:
-                # check which sensors need to be updated this cycle
-                processed_values = dict()
-                for sensor in self._cycle_triggers[self._cycle_count]:
-                    try:
-                        prop_name = sensor.measured_property_name
-                        value = prop_values[prop_name]
-                        processed_value = sensor.process_sample(value)
-                        processed_values[prop_name] = processed_value
-                    except KeyError:
-                        raise MissingPropertyError(
-                            'Missing expected update for property '
-                            f'{sensor.measured_property_name}!')
+            # calculate cycle triggers
+            # Example: if a 200 Hz sensor is attached to a plant which
+            # updates at 600 Hz, that means that for each sensor cycle,
+            # 3 plant cycles need to have passed. So, using the plant cycle
+            # count as reference, the sensor needs to sample at cycles:
+            # [0, 3, 6, 8, 12, ..., 600]
+            pcycles_per_scycle = \
+                self._plant_freq // sensor.sampling_frequency
+            for trigger in range(0, self._plant_freq, pcycles_per_scycle):
+                if trigger not in self._cycle_triggers:
+                    self._cycle_triggers[trigger] = []
 
-                # send updated values (if any) to commhandler
-                if len(processed_values) > 0:
-                    self._comm.send_sensor_values(processed_values)
-            except KeyError:
-                # no sensors on this cycle
-                pass
-            finally:
-                self._cycle_count = (self._cycle_count + 1) % self._plant_freq
+                self._cycle_triggers[trigger].append(sensor)
+
+    def process_plant_state(self,
+                            prop_values: Dict[str, PhyPropType]) \
+            -> Dict[str, PhyPropType]:
+        try:
+            # check which sensors need to be updated this cycle
+            processed_values = dict()
+            for sensor in self._cycle_triggers[self._cycle_count]:
+                try:
+                    prop_name = sensor.measured_property_name
+                    value = prop_values[prop_name]
+                    processed_value = sensor.process_sample(value)
+                    processed_values[prop_name] = processed_value
+                except KeyError:
+                    raise MissingPropertyError(
+                        'Missing expected update for property '
+                        f'{sensor.measured_property_name}!')
+
+            return processed_values
+        except KeyError:
+            # no sensors on this cycle
+            pass
+        finally:
+            self._cycle_count = (self._cycle_count + 1) % self._plant_freq
