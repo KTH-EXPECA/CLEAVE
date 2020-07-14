240c010cd88af80c314198507db0b164f08ddb24
Manuel Olguín Muñoz <manuel@olguin.se>
2020-07-14 15:18:08 -0400

    start implementing builder pattern for Plant creation

--- cleave/client/plant.py
+++ cleave/client/plant.py
@@ -1,47 +1,85 @@
 from __future__ import annotations
 
-from abc import ABC
+import warnings
+from abc import ABC, abstractmethod
+from typing import Dict
 
 from .actuator import Actuator
-from .sensor import Sensor
-from .state import State
+from .sensor import Sensor, SensorArray
 from ..network.handler import ClientCommHandler
+from ..util import PhyPropType
 
 
+class PlantBuilderWarning(Warning):
+    pass
+
+
+class State(ABC):
+    @abstractmethod
+    def advance(self,
+                dt_ns: int,
+                act_values: Dict[str, PhyPropType]):
+        pass
+
+
+class Plant(ABC):
+    # interface for plant
+    @abstractmethod
+    def execute(self):
+        pass
+
+    @abstractmethod
+    @property
+    def update_rate_hz(self) -> int:
+        pass
+
+    @abstractmethod
+    @property
+    def plant_state(self) -> State:
+        pass
+
+
+# noinspection PyAttributeOutsideInit
 class PlantBuilder:
-    # noinspection PyAttributeOutsideInit
     def reset(self):
         self._sensors = []
         self._actuators = []
         self._comm_handler = None
-        self._plant_freq = -1
         self._plant_state = None
 
     def __init__(self):
         self.reset()
 
     def attach_sensor(self, sensor: Sensor):
-        pass
+        self._sensors.append(sensor)
 
     def attach_actuator(self, actuator: Actuator):
-        pass
+        self._actuators.append(actuator)
 
-    def attach_comm_handler(self, handler: ClientCommHandler):
-        pass
+    def set_comm_handler(self, handler: ClientCommHandler):
+        if self._comm_handler is not None:
+            warnings.warn(
+                'Replacing already set ClientCommHandler for plant.',
+                PlantBuilderWarning
+            )
 
-    def set_update_rate(self, rate_hz: int):
-        pass
+        self._comm_handler = handler
 
     def set_plant_state(self, plant_state: State):
-        pass
+        if self._plant_state is not None:
+            warnings.warn(
+                'Replacing already set State for plant.',
+                PlantBuilderWarning
+            )
 
-    def build(self) -> Plant:
+        self._plant_state = plant_state
+
+    def build(self, plant_upd_freq: int) -> Plant:
         # TODO: build plant and reset
+        sensor_array = SensorArray(
+            plant_freq=plant_upd_freq,
+            sensors=self._sensors
+        )
+
         self.reset()
-        pass
-
-
-class Plant(ABC):
-    # interface for plant
-    def execute(self):
         pass
