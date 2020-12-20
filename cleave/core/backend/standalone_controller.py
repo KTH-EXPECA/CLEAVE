23590c46631de09235944d2e63fa0c9b6d755aff
Manuel Olguín Muñoz <manuel@olguin.se>
2020-12-20 12:59:45 -0300

    standalone controller, receives parameters as json string from stdin, logs to a file and outputs the interface and port as a json string on stdout

---
+++ cleave/core/backend/standalone_controller.py
@@ -0,0 +1,104 @@
+#  Copyright (c) 2020 KTH Royal Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import importlib
+import json
+from pathlib import Path
+from typing import Any, Mapping
+
+import click
+import loguru
+from jsonschema import ValidationError, validate
+from twisted.internet.posixbase import PosixReactorBase
+
+from cleave.core.backend import schemas
+from cleave.core.network.backend import UDPControllerService
+
+
+class JSON(click.ParamType):
+    name = 'JSON'
+
+    def convert(self, value, param, ctx):
+        try:
+            config = json.loads(value)
+            validate(config, schemas.standalone_controller)
+            return config
+        except json.JSONDecodeError:
+            self.fail(
+                f'failed to decode param "{value}" as a JSON object.',
+                param,
+                ctx
+            )
+        except ValidationError as e:
+            self.fail(
+                f'JSON failed validation: {e.message}.',
+                param,
+                ctx
+            )
+
+
+@click.command()
+@click.argument('config',
+                type=JSON())
+def run_controller(config: Mapping[str, Any]) -> None:
+    """
+    Runs a standalone controller. Takes a single positional argument 
+    corresponding to a JSON string adhering to the following schema:
+
+
+    {
+
+        'controller': 'string',
+
+        'module': 'string',
+
+        'params': 'object',
+
+        'uuid': 'string'
+
+    }
+    """
+
+    from twisted.internet import reactor
+    reactor: PosixReactorBase = reactor
+
+    ctrl_mod = importlib.import_module(config['module'])
+    ctrl_class = getattr(ctrl_mod, config['controller'])
+    params = config['params']
+    uuid = config['uuid']
+
+    controller = ctrl_class(**params)
+    path = Path(f'/tmp/controllers/{uuid}').resolve()
+    service = UDPControllerService(
+        controller=controller,
+        output_dir=path)
+
+    loguru.logger.add(path / 'controller.log',
+                      level=10,
+                      colorize=False,
+                      format='<light-green>{time}</light-green> '
+                             '<level><b>{level}</b></level> '
+                             '{message}')
+
+    port = reactor.listenUDP(0, service.protocol).getHost()
+    # out_q.put((port.interface, port.port, path))
+    print(
+        json.dumps({'interface': port.host, \
+                    'port'     : port.port},
+                   indent=None, separators=(',', ':'))
+    )
+    reactor.run()
+
+
+if __name__ == '__main__':
+    run_controller()
