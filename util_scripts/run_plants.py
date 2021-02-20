e7aae5864590124fbd1721eb809045a767c7c10e
Manuel Olguín Muñoz <manuel@olguin.se>
2021-02-20 16:57:55 -0500

    typo

--- util_scripts/run_plants.py
+++ util_scripts/run_plants.py
@@ -12,6 +12,18 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 import os
 import subprocess
 
@@ -23,7 +35,7 @@ import click
 @click.argument('num-plants', type=int)
 @click.option('--plant-addr-template',
               type=str, default='192.168.1.2{:02d}', show_default=True)
-@click.option('--cleave_docker_image',
+@click.option('--cleave_docker_img',
               type=str, default='molguin/cleave:cleave', show_default=True)
 @click.option('-d', '--exp-duration',
               type=str, default='35m', show_default=True)
