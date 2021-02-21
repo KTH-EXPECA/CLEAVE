5c751ab0035569a5d8feedd01e0b9230fec36249
Manuel Olguín Muñoz <manuel@olguin.se>
2021-02-21 12:28:47 -0500

    script to fetch all results automatically and easily

---
+++ util_scripts/collect_experiment_results.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#  Copyright (c) 2021 KTH Royal Institute of Technology
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import shutil
+import subprocess
+from pathlib import Path
+
+import click
+
+
+@click.command()
+@click.argument('controller-output', type=str)
+@click.argument('num-plants', type=int)
+@click.argument('plant-output', type=str)
+@click.argument('target-dir', type=str)
+@click.option('-a', '--plant-address-template', type=str,
+              default='192.168.1.2{:02d}', show_default=True)
+@click.option('-u', '--rpi-user', type=str,
+              default='pi', show_default=True)
+@click.option('-p', '--rpi-passwd', type=str,
+              default='raspberry', show_default=True)
+def main(controller_output: str,
+         num_plants: int,
+         plant_output: str,
+         target_dir: str,
+         plant_addr_template: str = '192.168.1.2{:02d}',
+         rpi_user: str = 'pi',
+         rpi_passwd: str = 'raspberry'):
+    # collect results per plant
+    ctrl_output = Path(controller_output).resolve()
+    base_output_dir = Path(target_dir).resolve()
+
+    for i in range(num_plants):
+        print(f'Collecting results for setup {i:d} out of {num_plants:d}')
+
+        # set up target dir
+        out_dir = base_output_dir / f'plant_{i:02d}'
+        out_dir.mkdir(parents=True, exist_ok=False)
+
+        # collect controller results
+        for ctrl_file in (ctrl_output / f'controller_{i:02d}').iterdir():
+            shutil.copy(ctrl_file.resolve(), out_dir)
+
+        # collect plant results
+        subprocess.run(
+            ['sshpass', '-p', rpi_passwd,
+             'scp', '-oPubkeyAuthentication=no', '-oPasswordAuthentication=yes',
+             f'{rpi_user}@{plant_addr_template.format(i)}:'
+             f'"{plant_output}/plant_{i:02d}/*"', out_dir]
+        )
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
